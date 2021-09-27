b76fdeee557687fe0d70b377abbe7ac5552821b1
Adam Harpur <a.harpur@axial3d.com>
2021-09-27 16:40:47 +0100

    feat: add play pause

--- www/index.js
+++ www/index.js
@@ -1,13 +1,39 @@
 import { Universe } from "wasm-game-of-life";
 
 const pre = document.getElementById("game-of-life-canvas");
+const playPauseButton = document.getElementById("play-pause");
+
 const universe = Universe.new();
 
+let animationId = null;
 
 function renderLoop(){
     pre.textContent = universe.render();
     universe.tick();
-    requestAnimationFrame(renderLoop);
+    animationId = requestAnimationFrame(renderLoop);
 };
 
-requestAnimationFrame(renderLoop);
+function isPaused(){
+    return animationId === null
+}
+
+function pause(){
+    playPauseButton.textContent = "▶";
+    cancelAnimationFrame(animationId);
+    animationId = null;
+}
+
+function play(){
+    playPauseButton.textContent = "⏸";
+    renderLoop();
+}
+
+playPauseButton.addEventListener("click", event => {
+    if (isPaused()) {
+        play();
+        return
+    }
+    pause();
+});
+
+play();
