7bec38afaa4feb7a7df204d95a9a4d5a03cc6d83
Adam Harpur <a.harpur@axial3d.com>
2021-09-29 10:00:50 +0100

    test: universe tick

--- tests/web.rs
+++ tests/web.rs
@@ -1,4 +1,3 @@
-//! Test suite for the Web and headless browsers.
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
@@ -8,7 +7,33 @@ use wasm_way_of_life::Universe;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
+
+#[cfg(test)]
+pub fn generate_input() -> Universe {
+    let mut universe = Universe::new();
+    universe.set_width(6);
+    universe.set_height(6);
+    universe.set_cells(&[(1,2), (2,3), (3,1), (3,2), (3,3)]);
+    universe
+}
+
+#[cfg(test)]
+pub fn generate_expected_after_one_tick() -> Universe {
+    let mut universe = Universe::new();
+    universe.set_width(6);
+    universe.set_height(6);
+    universe.set_cells(&[(2,1), (2,3), (3,2), (3,3), (4,2)]);
+    universe
+}
+
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+pub fn test_tick() {
+    // Let's create a smaller Universe with a small spaceship to test!
+    let mut input_universe = generate_input();
+
+    let expected_universe = generate_expected_after_one_tick();
+
+    // Call `tick` and then see if the cells in the `Universe`s are the same.
+    input_universe.tick();
+    assert_eq!(&input_universe.get_cells(), &expected_universe.get_cells());
 }
